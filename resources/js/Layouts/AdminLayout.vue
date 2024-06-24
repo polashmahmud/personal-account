58ce4bf46f9f3c93f5645da9285a0fddcde61d2d
Polash Mahmud <polashmahmud@gmail.com>
2024-06-24 21:46:32 +0600

    add Admin Layout

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="grid grid-cols-7 gap-6">
+                <div class="col-span-2">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">Sidebar</div>
+                    </div>
+                </div>
+                <div class="col-span-5">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import AdminLayout from "@/Layouts/AdminLayout.vue";
 </script>
 
 <template>
-<div>
-    Index
-</div>
+    <AdminLayout>
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">Main</div>
+        </div>
+    </AdminLayout>
 </template>
