bd3f98d07eb598b80909fed3be6c790387fb3632
Polash Mahmud <polashmahmud@gmail.com>
2024-06-30 20:56:51 +0600

    add bazar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BazarResource;
+use App\Models\Bazar;
+
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return inertia()->render('Home');
+        return inertia()->render('Home', [
+            'bazars' => BazarResource::collection(Bazar::latest()->get())
+        ]);
     }
 }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,6 +2,12 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/vue3';
 import SecondaryButton from "@/Components/SecondaryButton.vue";
+
+defineProps({
+    bazars: {
+        type: Object
+    }
+})
 </script>
 
 <template>
@@ -18,162 +24,21 @@ import SecondaryButton from "@/Components/SecondaryButton.vue";
 
         <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-6">
-                <div class="border border-gray-700 p-4 rounded">
+                <div
+                    v-for="bazar in bazars"
+                    :key="bazar.id"
+                    class="border border-gray-700 p-4 rounded"
+                >
                     <div class="flex space-x-3 text-gray-400">
                         <div class="w-20 flex-none">
                             <img class="w-full h-auto object-cover rounded-lg"
-                                 src="https://uttarakachabazar.com/wp-content/uploads/2022/12/eggplant_long-removebg-preview-copy.jpg"
+                                 :src="bazar.image"
                                  alt="">
                         </div>
                         <div class="relative w-full">
                             <div class="absolute top-0 right-0 bg-gray-600 py-1 px-2 rounded-lg text-xs text-white">১ কেজি</div>
-                            <h3 class="text-gray-200 font-semibold text-xl">আলু</h3>
-                            <p>মূল্য: ১০০ টাকা</p>
-                            <div class="items-center space-x-3">
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M12 4.5v15m7.5-7.5h-15"/>
-                                    </svg>
-                                </SecondaryButton>
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14"/>
-                                    </svg>
-                                </SecondaryButton>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="border border-gray-700 p-4 rounded">
-                    <div class="flex space-x-3 text-gray-400">
-                        <div class="w-20 flex-none">
-                            <img class="w-full h-auto object-cover rounded-lg"
-                                 src="https://uttarakachabazar.com/wp-content/uploads/2022/12/eggplant_long-removebg-preview-copy.jpg"
-                                 alt="">
-                        </div>
-                        <div class="relative w-full">
-                            <div class="absolute top-0 right-0 bg-gray-600 py-1 px-2 rounded-lg text-xs text-white">১ কেজি</div>
-                            <h3 class="text-gray-200 font-semibold text-xl">আলু</h3>
-                            <p>মূল্য: ১০০ টাকা</p>
-                            <div class="items-center space-x-3">
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M12 4.5v15m7.5-7.5h-15"/>
-                                    </svg>
-                                </SecondaryButton>
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14"/>
-                                    </svg>
-                                </SecondaryButton>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="border border-gray-700 p-4 rounded">
-                    <div class="flex space-x-3 text-gray-400">
-                        <div class="w-20 flex-none">
-                            <img class="w-full h-auto object-cover rounded-lg"
-                                 src="https://uttarakachabazar.com/wp-content/uploads/2022/12/eggplant_long-removebg-preview-copy.jpg"
-                                 alt="">
-                        </div>
-                        <div class="relative w-full">
-                            <div class="absolute top-0 right-0 bg-gray-600 py-1 px-2 rounded-lg text-xs text-white">১ কেজি</div>
-                            <h3 class="text-gray-200 font-semibold text-xl">আলু</h3>
-                            <p>মূল্য: ১০০ টাকা</p>
-                            <div class="items-center space-x-3">
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M12 4.5v15m7.5-7.5h-15"/>
-                                    </svg>
-                                </SecondaryButton>
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14"/>
-                                    </svg>
-                                </SecondaryButton>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="border border-gray-700 p-4 rounded">
-                    <div class="flex space-x-3 text-gray-400">
-                        <div class="w-20 flex-none">
-                            <img class="w-full h-auto object-cover rounded-lg"
-                                 src="https://uttarakachabazar.com/wp-content/uploads/2022/12/eggplant_long-removebg-preview-copy.jpg"
-                                 alt="">
-                        </div>
-                        <div class="relative w-full">
-                            <div class="absolute top-0 right-0 bg-gray-600 py-1 px-2 rounded-lg text-xs text-white">১ কেজি</div>
-                            <h3 class="text-gray-200 font-semibold text-xl">আলু</h3>
-                            <p>মূল্য: ১০০ টাকা</p>
-                            <div class="items-center space-x-3">
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M12 4.5v15m7.5-7.5h-15"/>
-                                    </svg>
-                                </SecondaryButton>
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14"/>
-                                    </svg>
-                                </SecondaryButton>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="border border-gray-700 p-4 rounded">
-                    <div class="flex space-x-3 text-gray-400">
-                        <div class="w-20 flex-none">
-                            <img class="w-full h-auto object-cover rounded-lg"
-                                 src="https://uttarakachabazar.com/wp-content/uploads/2022/12/eggplant_long-removebg-preview-copy.jpg"
-                                 alt="">
-                        </div>
-                        <div class="relative w-full">
-                            <div class="absolute top-0 right-0 bg-gray-600 py-1 px-2 rounded-lg text-xs text-white">১ কেজি</div>
-                            <h3 class="text-gray-200 font-semibold text-xl">আলু</h3>
-                            <p>মূল্য: ১০০ টাকা</p>
-                            <div class="items-center space-x-3">
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                              d="M12 4.5v15m7.5-7.5h-15"/>
-                                    </svg>
-                                </SecondaryButton>
-                                <SecondaryButton class="mt-3">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14"/>
-                                    </svg>
-                                </SecondaryButton>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="border border-gray-700 p-4 rounded">
-                    <div class="flex space-x-3 text-gray-400">
-                        <div class="w-20 flex-none">
-                            <img class="w-full h-auto object-cover rounded-lg"
-                                 src="https://uttarakachabazar.com/wp-content/uploads/2022/12/eggplant_long-removebg-preview-copy.jpg"
-                                 alt="">
-                        </div>
-                        <div class="relative w-full">
-                            <div class="absolute top-0 right-0 bg-gray-600 py-1 px-2 rounded-lg text-xs text-white">১ কেজি</div>
-                            <h3 class="text-gray-200 font-semibold text-xl">আলু</h3>
-                            <p>মূল্য: ১০০ টাকা</p>
+                            <h3 class="text-gray-200 font-semibold text-xl">{{ bazar.title }}</h3>
+                            <p>মূল্য: {{ bazar.amount }} টাকা</p>
                             <div class="items-center space-x-3">
                                 <SecondaryButton class="mt-3">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
